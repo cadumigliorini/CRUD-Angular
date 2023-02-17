6f05ab799080151df0113bbe44e7f2eb057b867d
Carlos Migliorini <cadumigliorini@hotmail.com>
2023-02-17 08:58:41 -0300

    projeto finalizado

---
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,24 @@
+import { HeaderService } from './../../components/template/header/header.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent {
+
+  constructor(private headerService: HeaderService, private router: Router){
+    headerService.headerData = {
+      title: "Cadastro de Produtos",
+      icon: "storefront",
+      routeUrl: "/products"
+    }
+  }
+
+  navigateToProductCreate() : void {
+      
+      this.router.navigate(['/products/create']);
+  }
+}
